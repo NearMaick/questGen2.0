c56b7753e786bef1a80ef20d41feed9b644b7d51
Maick Souza <maick_a_s@msn.com>
2020-08-28 16:07:50 -0300

    feat(web): question-find #1

--- src/pages/QuestionFind/index.tsx
+++ src/pages/QuestionFind/index.tsx
@@ -1,7 +1,7 @@
 import React, { useRef, useCallback } from 'react';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
-import { Container } from './styles';
+import { Container, SearchFormContainer, SearchIdContainer } from './styles';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
@@ -12,17 +12,34 @@ interface QuestionFormData {
 const QuestionFind: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
-  const handleSubmit = useCallback((data: QuestionFormData) => {
+  const handleSearchSubmit = useCallback((data: QuestionFormData) => {
+    console.log(data);
+  }, []);
+
+  const handleSearchIdSubmit = useCallback((data: QuestionFormData) => {
     console.log(data);
   }, []);
 
   return (
     <Container>
-      <h1>Bem vindo, professor Maick Souza</h1>
-      <Form ref={formRef} onSubmit={handleSubmit}>
-        <Input name="question" placeholder="Digite sua pesquisa aqui" />
-        <Button type="submit">Enviar</Button>
-      </Form>
+      <h2>Pesquisar por</h2>
+      <SearchFormContainer>
+        <Form ref={formRef} onSubmit={handleSearchSubmit}>
+          <Input name="course" placeholder="Curso" />
+          <Input name="subject" placeholder="Matéria" />
+          <Input name="level" placeholder="Peso da questão" />
+          <Input name="quantity" placeholder="Quantidade de questões" />
+          <Button type="submit">Enviar</Button>
+        </Form>
+      </SearchFormContainer>
+      <h2>ou</h2>
+      <h2>Listar por id</h2>
+      <SearchIdContainer>
+        <Form ref={formRef} onSubmit={handleSearchIdSubmit}>
+          <Input name="listQuestionId" placeholder="Id da lista de questões" />
+          <Button type="submit">Enviar</Button>
+        </Form>
+      </SearchIdContainer>
     </Container>
   );
 };
--- src/pages/QuestionFind/styles.ts
+++ src/pages/QuestionFind/styles.ts
@@ -1,3 +1,7 @@
 import styled from 'styled-components';
 
 export const Container = styled.div``;
+
+export const SearchFormContainer = styled.div``;
+
+export const SearchIdContainer = styled.div``;
